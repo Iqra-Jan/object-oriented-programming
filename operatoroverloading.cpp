6616c405f2a817af80827b099f72905129697127
Iqra <iqranjan9@gmail.com>
2025-11-27 15:43:26 +0530

    Add files via upload

---
+++ operatoroverloading.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Number {
+    int value;
+
+public:
+    Number(int v) : value(v) {}
+
+    void operator ++() {     
+        value = value + 1;
+    }
+
+    void display() {
+        cout << "Value: " << value << endl;
+    }
+};
+
+int main() {
+    Number n(5);
+
+    cout << "Before increment: ";
+    n.display();
+
+    ++n;  
+    cout << "After increment: ";
+    n.display();
+
+    return 0;
+}
